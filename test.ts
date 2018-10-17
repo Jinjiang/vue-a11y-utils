4ef14bacb79b84a2892b62b5e929dde9b4764657
Jinjiang <zhaojinjiang@me.com>
2018-10-18 03:19:26 +0800

    supported typescript + jest + vue

--- test.ts
+++ test.ts
@@ -1,7 +1,42 @@
+import Vue from 'vue';
+import { mount } from '@vue/test-utils';
+
 import VueA11y from './';
 
 describe('foo', () => {
   it('bar', () => {
-    expect(VueA11y).toEqual({});
+    expect(VueA11y.name).toBe('vue-a11y-utils');
+  });
+});
+
+describe('Counter', () => {
+  // Now mount the component and you have the wrapper
+  const Counter = Vue.extend({
+    template: `
+      <div>
+        <span class="count">0</span>
+        <button @click="increment">Increment</button>
+      </div>
+    `,
+    data() {
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      increment() {
+        this.count++;
+      }
+    }
+  });
+  const wrapper = mount(Counter);
+
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain('<span class="count">0</span>');
+  });
+
+  // it's also easy to check for the existence of elements
+  it('has a button', () => {
+    expect(wrapper.contains('button')).toBe(true);
   });
 });
