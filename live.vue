e5dc544a25dc3cadb0d99cd284f8ef2030f47f55
Jinjiang <zhaojinjiang@me.com>
2018-10-27 01:14:05 +0800

    removed console.log in source code by mistake

--- live.vue
+++ live.vue
@@ -32,7 +32,6 @@ const VueLiveInterface = Vue.extend({
     const self = this;
     return {
       announce(message: string, important: boolean, force: boolean) {
-        console.log("announce", message, important, force);
         if (force) {
           self.assertiveMessage = "";
           self.politeMessage = "";
