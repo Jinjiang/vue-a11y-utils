223f07828db205628e8793c46767b339b46d8c81
Jinjiang <zhaojinjiang@me.com>
2018-10-27 02:36:20 +0800

    created directories for source code & examples

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-export { VueAria, directiveAria } from "./aria";
-export { default as VueFocusTrap } from "./focus-trap.vue";
-export { default as MixinKeyTravel } from "./key-travel";
-export { default as MixinId } from "./id";
-export { default as MixinKeyShortcuts } from "./key-shortcuts";
-export { default as VueLive } from "./live.vue";
+export { VueAria, directiveAria } from "./src/aria";
+export { default as VueFocusTrap } from "./src/focus-trap.vue";
+export { default as MixinKeyTravel } from "./src/key-travel";
+export { default as MixinId } from "./src/id";
+export { default as MixinKeyShortcuts } from "./src/key-shortcuts";
+export { default as VueLive } from "./src/live.vue";
