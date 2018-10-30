7f55a175804bbbb23f1825ef04aec904372b4b62
Jinjiang <zhaojinjiang@me.com>
2018-10-31 00:15:07 +0800

    added disabled prop for VueFocusTrap component

--- src/focus-trap.vue
+++ src/focus-trap.vue
@@ -8,29 +8,54 @@
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({
+import Component from "vue-class-component";
+
+const VueFocusTrapInterface = Vue.extend({
+  props: {
+    disabled: Boolean
+  }
+});
+
+/**
+ * <VueFocusTrap disabled>
+ * - props: disabled
+ * - events: gofirst, golast
+ * - slots: default slot
+ */
+@Component({
+  data() {
+    return {
+      mounted: false
+    };
+  },
   mounted() {
+    this.mounted = true;
     document.addEventListener("focus", this.trapFocus, true);
   },
   beforeDestroy() {
-    document.removeEventListener("focus", this.trapFocus, true);
-  },
-  methods: {
-    trapFocus(event: FocusEvent) {
-      const root = this.$el;
-      const { start, end } = this.$refs;
-      const { target } = event;
-      if (!root.contains(<HTMLElement>target)) {
-        event.preventDefault();
-        this.$emit("gofirst");
-      } else if (target === start) {
-        event.preventDefault();
-        this.$emit("golast");
-      } else if (target === end) {
-        event.preventDefault();
-        this.$emit("gofirst");
-      }
+    if (this.mounted) {
+      document.removeEventListener("focus", this.trapFocus, true);
     }
   }
-});
+})
+export default class VueFocusTrap extends VueFocusTrapInterface {
+  trapFocus(event: FocusEvent) {
+    if (this.disabled) {
+      return;
+    }
+    const root = this.$el;
+    const { start, end } = this.$refs;
+    const { target } = event;
+    if (!root.contains(<HTMLElement>target)) {
+      event.preventDefault();
+      this.$emit("gofirst");
+    } else if (target === start) {
+      event.preventDefault();
+      this.$emit("golast");
+    } else if (target === end) {
+      event.preventDefault();
+      this.$emit("gofirst");
+    }
+  }
+}
 </script>
