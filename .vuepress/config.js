aa1796b1e18839f63fb6ad04b1f973e8059a6843
Jinjiang <zhaojinjiang@me.com>
2018-11-02 00:41:13 +0900

    updated vuepress config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  base: "/vue-a11y-utils/"
+  base: "/vue-a11y-utils/",
+  title: "Vue A11y Utils",
+  description: "Utilities for accessibility (a11y) in Vue.js",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/examples/" }
+    ],
+
+    sidebar: ["/"],
+    sidebarDepth: 1,
+
+    repo: "jinjiang/vue-a11y-utils",
+    repoLabel: "Contribute!",
+    docsDir: ".",
+    docsBranch: "master",
+    editLinks: true,
+    editLinkTest: "Help us improve this page!"
+  }
 };
