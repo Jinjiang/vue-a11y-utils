7891fe4c90362b74024140618bc19a66e7df8efd
Jinjiang <zhaojinjiang@me.com>
2018-11-14 17:29:49 +0800

    exported new MixinTravel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export { VueAria, directiveAria } from "./aria";
 export { default as VueFocusTrap } from "./focus-trap.vue";
-export { default as MixinKeyTravel } from "./key-travel";
+export { default as MixinTravel } from "./travel";
 export { default as MixinId } from "./id";
-export { default as MixinKeyShortcuts } from "./key-shortcuts";
+export { default as MixinShortcuts } from "./key-shortcuts";
 export { default as VueLive } from "./live.vue";
+
+// deprecated
+export { default as MixinKeyTravel } from "./key-travel";
+export { default as MixinKeyShortcuts } from "./key-shortcuts";
