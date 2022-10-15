cf6c80059301e609d98518e0f2a6719d509fc610
Jinjiang <zhaojinjiang@me.com>
2022-10-15 10:27:34 +0800

    support examples by vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { createVuePlugin as vue2 } from "vite-plugin-vue2";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue2()],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm.js"
+    }
+  }
+});
