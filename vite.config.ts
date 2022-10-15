d0761234ca0e20aef5b141010f1c97244bd606ed
Jinjiang <zhaojinjiang@me.com>
2022-10-15 15:03:24 +0800

    support live examples in docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import { createVuePlugin as vue2 } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue-a11y-utils/examples/",
   plugins: [vue2()],
   resolve: {
     alias: {
       vue: "vue/dist/vue.esm.js"
     }
+  },
+  build: {
+    outDir: "docs/examples"
   },
   test: {
     globals: true,
