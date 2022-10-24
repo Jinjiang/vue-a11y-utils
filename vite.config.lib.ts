55869c264dd319c6dce37ebffa830feb35951d60
Jinjiang <zhaojinjiang@me.com>
2022-10-24 15:07:45 +0800

    setup v2 -> v3

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,10 +1,10 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import { createVuePlugin as vue2 } from "vite-plugin-vue2";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue2()],
+  plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
-import { createVuePlugin as vue2 } from "vite-plugin-vue2";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-a11y-utils/examples/",
-  plugins: [vue2()],
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.esm.js"
-    }
-  },
-  build: {
-    outDir: "docs/examples"
-  },
-  test: {
-    globals: true,
-    environment: "jsdom"
-  }
+  plugins: [vue()]
 });
